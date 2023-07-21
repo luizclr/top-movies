b8a27ff719098ab1a6473e0c67c86e249ee28414
Luiz Claudio <luiz.lclr@gmail.com>
2023-07-21 12:19:03 -0300

    refactor(sidebar): remove <Sidebar />

--- src/components/sidebar/types.ts
+++
@@ -1,4 +0,0 @@
-export type BaseSidebarProps = {
-  handleLogoutClick: () => void;
-  handleChangeThemeClick: () => void;
-};
--- src/pages/base/base.tsx
+++ src/pages/base/base.tsx
@@ -2,13 +2,11 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 import ErrorBoundary from "~/components/error-boundary/error-boundary";
-import { Sidebar } from "~/components/sidebar/sidebar.container";
 import { BaseContent, BaseWrapper } from "~/pages/base/base.styles";
 
 const Base: React.FC = () => {
   return (
     <BaseWrapper>
-      <Sidebar />
       <BaseContent>
         <ErrorBoundary>
           <Suspense fallback={<div>loading...</div>}>
